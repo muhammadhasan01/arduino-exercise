11aa6947e494993f40bde2c07cb8b6266a1ee0e1
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-01-23 13:30:03 +0700

    add chapter 2 screenshots

--- w01_arduino_exercise/chapter_2/chapter_2_3/2_3_sketch.ino
+++ w01_arduino_exercise/chapter_2/chapter_2_3/2_3_sketch.ino
@@ -1,5 +1,5 @@
-const int k_numLEDs = 4;
-const int kPinLeds[k_numLEDs] = {2, 3, 5, 7};
+const int k_numLEDs = 7;
+const int kPinLeds[k_numLEDs] = {2, 3, 4, 5, 6, 7, 8};
 
 void setup() {
   for (int i = 0; i < k_numLEDs; i++) {

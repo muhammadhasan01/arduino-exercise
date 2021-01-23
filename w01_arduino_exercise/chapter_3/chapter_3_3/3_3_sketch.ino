1cf085762b05f8230e4fa9bb226e8e7d44c7bbe4
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-01-23 14:47:47 +0700

    add CHALLENGE chapter 3

---
+++ w01_arduino_exercise/chapter_3/chapter_3_3/3_3_sketch.ino
@@ -0,0 +1,25 @@
+const int kButtonFrom = 2;
+const int kButtonTo = 3;
+const int kPinLed_R = 9;
+const int kPinLed_G = 8;
+const int kPinLed_B = 7;
+
+void setup() {
+    pinMode(kButtonFrom, INPUT);
+    pinMode(kButtonTo, INPUT);
+    pinMode(kPinLed_R, OUTPUT);
+    pinMode(kPinLed_G, OUTPUT);
+    pinMode(kPinLed_B, OUTPUT);
+}
+
+int curValue = 0;
+
+void loop() {
+    if (digitalRead(kButtonFrom) == HIGH || digitalRead(kButtonTo) == HIGH) {
+        curValue = (curValue + 1) % 255;
+    }
+    analogWrite(kPinLed_R, curValue);
+    analogWrite(kPinLed_G, curValue);
+    analogWrite(kPinLed_B, curValue);
+  	delay(400);
+}
---
+++ w01_arduino_exercise/chapter_3/chapter_3_4/3_4_sketch.ino
@@ -0,0 +1,3 @@
+// Answer:
+// If you disassemble a compiled sketch, you can count the number of instructions, and from the processor datasheet, figure out the number of cycles for each instruction.
+// Cycles times one-over clock speed (62.5ns) gives time.

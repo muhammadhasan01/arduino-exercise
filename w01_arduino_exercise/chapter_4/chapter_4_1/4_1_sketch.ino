684e1e542fc6c682345ffe3e0e2579b1844be912
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-01-23 15:17:46 +0700

    fix exercise 1 in chapter 4

--- w01_arduino_exercise/chapter_4/chapter_4_1/4_1_sketch.ino
+++ w01_arduino_exercise/chapter_4/chapter_4_1/4_1_sketch.ino
@@ -9,33 +9,21 @@ const int NOTE_C5 = 523;
 const int pinSpeaker = 10;
 
 const int HAPPY_BIRTHDAY[6][2] = {
-  {NOTE_C4, 1000},
-  {NOTE_C4, 1000},
-  {NOTE_D4, 2000},
-  {NOTE_C4, 2000},
-  {NOTE_F4, 2000},
-  {NOTE_E4, 4000},
+	{NOTE_C4, 1000},
+	{NOTE_C4, 1000},
+	{NOTE_D4, 2000},
+	{NOTE_C4, 2000},
+	{NOTE_F4, 2000},
+	{NOTE_E4, 4000},
 };
 
-const int k_Pinbutton_1 = 2;
-const int k_Pinbutton_2 = 3;
-
 void setup() { 
-	pinMode(k_Pinbutton_1, INPUT);
-	pinMode(k_Pinbutton_2, INPUT);
 	pinMode(pinSpeaker, OUTPUT);
 }
 
 void loop() {
-	int value = 1000;
-	if (digitalRead(k_Pinbutton_1) == LOW) {
-		value -= 500;
-	}
-	if (digitalRead(k_Pinbutton_2) == LOW) {
-		value -= 400;
-	}
 	for (int i = 0; i < 6; i++) {
-		tone(HAPPY_BIRTHDAY[i][0] % value, value, HAPPY_BIRTHDAY[0][1]);
+		tone(HAPPY_BIRTHDAY[i][0], HAPPY_BIRTHDAY[0][1], 500);
 		delay(400);
 	}
 	delay(2000);

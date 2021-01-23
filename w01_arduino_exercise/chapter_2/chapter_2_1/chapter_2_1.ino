54be2fc29b6c6bbd4c78e34e7ab4cce1fa161e79
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-01-23 11:24:14 +0700

    add chapter_2_1.ino

---
+++ w01_arduino_exercise/chapter_2/chapter_2_1/chapter_2_1.ino
@@ -0,0 +1,15 @@
+const int kPinLed = 13;
+
+void setup() {
+  pinMode(kPinLed, OUTPUT);
+}
+
+void loop() {
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(kPinLed, HIGH);
+    delay(200);
+    digitalWrite(kPinLed, LOW);
+    delay(200);
+  }
+  delay(1000); // 1 second
+}
